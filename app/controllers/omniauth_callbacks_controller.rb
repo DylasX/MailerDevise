63cfe8fabf7acf69b0178437a2f6986a8b78b4c6
Dylas <miguel-sg01@hotmail.com>
2016-07-28 17:00:43 -0500

    finalizado twitter

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,2 +1,19 @@
-class OmniauthCallbacksController < ApplicationController
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
+  def all
+    user = User.from_omniauth(request.env["omniauth.auth"])
+    session["devise.user_attributes"]= user.attributes
+    if user.persisted?
+      flash[:notice] = "Successfully signed in!"
+      sign_in_and_redirect user
+    else
+
+      redirect_to new_user_registration_path
+
+    end
+  end
+
+  alias_method :twitter, :all
+
+
 end
