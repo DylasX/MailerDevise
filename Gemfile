c438cf9baa364b6cda14da8d73b4103ee099c64d
Dylas <miguel-sg01@hotmail.com>
2016-07-28 15:35:06 -0500

    Twitter

--- Gemfile
+++ Gemfile
@@ -16,6 +16,8 @@ gem 'coffee-rails', '~> 4.1.0'
 gem 'devise'
 
 gem 'materialize-sass'
+gem 'omniauth'
+gem 'omniauth-twitter'
 
 
 # Use jquery as the JavaScript library
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,4 +271,5 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+  config.omniauth :twitter, 'ja3DaM7hBnJmt2sDFd3ZZ8PIq', 'NEjl65gAytxRQa4WtkrxqSKH65a3ZmQVNBk5zX3YtTlACLk4cl'
 end
